42d0907aeaf89f9abbef028b42e3e0024efcb1c7
giddudink <giddudink@gmail.com>
2020-11-10 23:19:46 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='panphon',
-      version='0.18.1-alpha',
+      version='0.18.2-alpha',
       description='Tools for using the International Phonetic Alphabet with phonological features',
       url='https://github.com/giddudink/panphon',
       download_url='https://github.com/giddudink/panphon/archive/0.18.1-alpha.tar.gz',
